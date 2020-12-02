40692c8d82ebe15c370b53d6890906793bf72d70
andersderkgardenas <anders.gardenas@gmail.com>
2020-12-02 18:47:58 +0100

    Completed advent 2

--- app/src/main/kotlin/advent/app/App2_2.kt
+++ app/src/main/kotlin/advent/app/App2_2.kt
@@ -6,17 +6,17 @@ package advent.app
 
 fun main() {
     val file = fileToStringList("app/src/main/kotlin/input/input2_1.txt")
-    val matches = file.count{ validPassword(it) }
+    val matches = file.count{ validPassword2(it) }
     println(matches)
 
 }
 
-fun validPassword(line: String): Boolean {
+fun validPassword2(line: String): Boolean {
     val splitLine = line.split(" ")
-    val min = splitLine[0].split("-")[0].toInt()
-    val max = splitLine[0].split("-")[1].toInt()
+    val min = splitLine[0].split("-")[0].toInt() -1
+    val max = splitLine[0].split("-")[1].toInt() -1
     val token = splitLine[1].first()
     val password = splitLine[2]
-    val matches = password.filter{ it == token }.count()
-    return matches in min..max
+    val toCharArray = password.toCharArray()
+    return (toCharArray[min] == token).xor(toCharArray[max] == token)
 }
